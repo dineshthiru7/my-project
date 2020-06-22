a1313a556900b58c3a2d511066fd7afdce15c1dd
MINDGATE <MINDGATE@MGSCHN-LAP-016>
2020-06-22 06:27:03 -0600

    committ all

---
+++ sampleone/src/sampleone/Demo.java
@@ -0,0 +1,9 @@
+package sampleone;
+
+public class Demo {
+	public static void main(String[] args) {
+
+		System.out.println("haiii hellooo.......");
+		System.out.println("haiii hellooo.......");
+	}
+}
